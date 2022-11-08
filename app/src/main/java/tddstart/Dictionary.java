23c90e3c725456667d12876feda9be714fd97d10
elodie-saintaubin <backup.esa00@gmail.com>
2022-11-08 16:31:10 +0100

    "Correction de bugs dans Triangulation"

--- app/src/main/java/tddstart/Dictionary.java
+++ app/src/main/java/tddstart/Dictionary.java
@@ -5,25 +5,37 @@ import java.util.*;
 //test git
 public class Dictionary {
     private String name;
-    private Map<String, String> translations;
+    private Map<String, String> translations = new HashMap<>();
 
-    Dictionary(String nom){ this.name = nom;}
-    Dictionary(){
+    private ArrayList<Map<String, String>> dictionnaire;
+
+    Dictionary(String name){ this.name = name;}
+    public Dictionary(){
         this.name = "default";
+    }
+
+    public Dictionary(Map<String, String> d){
+        this.translations = d;
+    }
+
+    public Dictionary(ArrayList<Map<String, String>> d){
+        this.dictionnaire = d;
     }
 
     //public String getName(){return "Example";};
     public String getName(){return this.name;}
 
-    public void addTranslation(String word, String translation){
-        translations.put(word, translation);
-    }
 
-    // Triangulation - q4
+    public void addTranslation(String word, String translation){
+        this.translations.put(word, translation);
+    }
     public String getTranslation(String word){
-        //TODO
+        if(this.translations == null)
+            return null;
         return translations.get(word);
     }
+
+
     //2) Add sans get : Non
     //3) Get sans add : oui partiellement à condition que ce soit un mock
     public boolean isEmpty(){
--- app/src/test/java/tddstart/DictionaryTest.java
+++ app/src/test/java/tddstart/DictionaryTest.java
@@ -1,27 +1,26 @@
 package tddstart;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
 
 public class DictionaryTest extends AppTest{
 
-    Dictionary dict = new Dictionary("");
+    Dictionary dict = new Dictionary();
 
-
+    @Before
+    public void initialize () {
+        dict = new Dictionary("Example");
+    }
     @Test public void testDictionaryName() {
         assertThat(dict.getName(), equalTo("Example"));
     }
 
-    //@Test public void testOneTranslation() {
-      //  dict.addTranslation("contre", "against");
-        //assertThat(dict.getTranslation("contre"), equalsTo("against"));
-    //}
-
-    //7
     @Test public void testIsEmpty() {
         assertThat(dict.isEmpty(), equalTo(true));
     }
@@ -32,16 +31,13 @@ public class DictionaryTest extends AppTest{
         assertThat(dict.getTranslation("contre"), equalTo("against"));
     }
 
-    //Triangulation 2
-    @Test public void testOneTranslation2() {
-        assertThat(dict.getTranslation("contre"), equalTo("against"));
-    }
 
     //Triangulation 4
     @Test public void testOneTranslation4() {
-        assertThat(dict.getTranslation("contrre"), equalTo("against"));
-        assertThat(dict.getTranslation("pareil"), equalTo("same"));
-
+        Map<String, String> d = new HashMap<>();
+        Dictionary dict4 = new Dictionary(d);
+        dict4.addTranslation("contrre", "against");
+        assertThat(dict4.getTranslation("contrre"), equalTo("against"));
     }
 
 
