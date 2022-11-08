988e26ae9346ecd17205b4aa1481477c2f1e1793
elodie-saintaubin <backup.esa00@gmail.com>
2022-11-08 15:41:35 +0100

    "question 3"

--- app/src/main/java/tddstart/Dictionary.java
+++ app/src/main/java/tddstart/Dictionary.java
@@ -4,25 +4,24 @@ import java.util.*;
 
 //test git
 public class Dictionary {
-    private String nom;
-    Dictionary(String nom){
-        this.nom = nom;
-    }
+    private String name;
+
+    Dictionary(String nom){ this.name = nom;}
     Dictionary(){
-        this.nom = "default";
+        this.name = "default";
     }
 
-    public String getName(){
-        return this.nom;
-    }
+    //public String getName(){return "Example";};
+    public String getName(){return this.name;}
 
     public void addTranslation(String word, String translation){
 
     }
 
+    // Triangulation - q4
     public String getTranslation(String word){
         //TODO
-        return "";
+        return "against";
     }
     //2) Add sans get : Non
     //3) Get sans add : oui partiellement à condition que ce soit un mock
--- app/src/test/java/tddstart/DictionaryTest.java
+++ app/src/test/java/tddstart/DictionaryTest.java
@@ -3,6 +3,9 @@ package tddstart;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class DictionaryTest extends AppTest{
 
@@ -21,6 +24,27 @@ public class DictionaryTest extends AppTest{
     @Test public void testIsEmpty() {
         assertThat(dict.isEmpty(), equalTo(true));
     }
+
+
+    @Test public void testOneTranslation() {
+        dict.addTranslation("contre", "against");
+        assertThat(dict.getTranslation("contre"), equalTo("against"));
+    }
+
+    //Triangulation 2
+    @Test public void testOneTranslation2() {
+        assertThat(dict.getTranslation("contre"), equalTo("against"));
+    }
+
+    //Triangulation 4
+    @Test public void testOneTranslation4() {
+        assertThat(dict.getTranslation("contrre"), equalTo("against"));
+
+    }
+
+
+
+
 
 
 }
